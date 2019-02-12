265d3416e9e51f37083a201b61a2589fde3e55e8
Jimmy Jacobson <jimmy6727@gmail.com>
2019-02-12 14:24:50 +0000

    Tuesday afternoon, Driver map activity and Customer map activity gets location on resume and without uninstall :)

--- app/src/main/java/com/example/uberjim/CustomerMapActivity.java
+++ app/src/main/java/com/example/uberjim/CustomerMapActivity.java
@@ -75,6 +75,39 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
         });
 
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        onResume(mMap);
+    }
+
+    protected void onResume(GoogleMap googleMap){
+        mMap = googleMap;
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // Permission check
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                checkLocationPermission();
+            } else {
+                checkLocationPermission();
+            }
+        }
+
+    }
 // ..
 
 
@@ -99,7 +132,7 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
+                mMap.setMyLocationEnabled(true);
             } else {
                 checkLocationPermission();
             }
